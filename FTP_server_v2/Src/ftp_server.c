b437a52f46f0c46068d96d7408cd5c0342ad94c0
Adrian Beściak <adrian.besciak99@gmail.com>
2021-01-09 23:22:44 +0100

    added talk with ftp client

--- FTP_server_v2/Src/ftp_server.c
+++ FTP_server_v2/Src/ftp_server.c
@@ -22,17 +22,76 @@ UART_HandleTypeDef * huart;
 static void ftp_server_serve(struct netconn * conn) {
 	struct netbuf * inbuf;
 	err_t recv_err;
-	char* buf;
+	char buf[50];
 	u16_t buflen;
 
-	char logbuf[100];
+	char logbuf[50];
 
-	char hello_response[50];
-	sprintf(hello_response, "220");
+	/* initial response */
+	char hello_response[20];
+	sprintf(hello_response, "220 (vsFTPd 3.0.3)\r\n");
 	netconn_write(conn, hello_response, sizeof(hello_response), NETCONN_NOCOPY);
-	//sprintf(hello_response, "230");
-	//netconn_write(conn, hello_response, sizeof(hello_response), NETCONN_NOCOPY);
 
+	recv_err = netconn_recv(conn, &inbuf);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			//vTaskDelay(300);
+		}
+	}
+
+	/* user credentials request */
+	char response2[38];
+	sprintf(response2, "530 Please login with USER and PASS.\r\n");
+	netconn_write(conn, response2, sizeof(response2), NETCONN_NOCOPY);
+
+	//vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			//vTaskDelay(300);
+		}
+	}
+
+	/* second user credentials request the same as previously */
+	//sprintf(response2, "530 Please login with USER and PASS.\r\n");
+	netconn_write(conn, response2, sizeof(response2), NETCONN_NOCOPY);
+
+	//vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			//vTaskDelay(300);
+		}
+	}
+
+	/* received user name, ask for password */
+	char response3[34];
+	sprintf(response3, "331 Please specify the password.\r\n");
+	netconn_write(conn, response3, sizeof(response3), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
 	recv_err = netconn_recv(conn, &inbuf);
 	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
 	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
@@ -47,6 +106,163 @@ static void ftp_server_serve(struct netconn * conn) {
 			vTaskDelay(300);
 		}
 	}
+
+	/* send message about succesfull login */
+	char response4[23];
+	sprintf(response4, "230 Login successful.\r\n");
+	netconn_write(conn, response4, sizeof(response4), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			vTaskDelay(300);
+		}
+	}
+
+	/* send message about system type */
+/*	char response5[15];
+	sprintf(response5, "215 NAME stm.\r\n");
+	netconn_write(conn, response5, sizeof(response5), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			vTaskDelay(300);
+		}
+	}*/
+
+	/* respond to FEAT command that i dont know it */
+/*	char response6[5];
+	sprintf(response6, "500\r\n");
+	netconn_write(conn, response6, sizeof(response6), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			vTaskDelay(300);
+		}
+	}*/
+
+	/* respond with the current directory */
+	char response7[34];
+	sprintf(response7, "257 \"/\" is the current dierctory\r\n");
+	netconn_write(conn, response7, sizeof(response7), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			vTaskDelay(300);
+		}
+	}
+
+	/*set binary mode */
+	char response8[30];
+	sprintf(response8, "200 Switching to Binary mode.\r\n");
+	netconn_write(conn, response8, sizeof(response8), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			vTaskDelay(300);
+		}
+	}
+
+	/* passive mode */
+	char response9[48];
+	sprintf(response9, "227 Entering Passive Mode (172,16,25,125,0,23).\r\n");
+	netconn_write(conn, response9, sizeof(response9), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			vTaskDelay(300);
+		}
+	}
+
+	/* tell that transmission has been started */
+	char response10[39];
+	sprintf(response10, "150 Here comes the directory listing.\r\n");
+	netconn_write(conn, response10, sizeof(response10), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
+	/*here should be data transmission on data port */
+
+
+	/* tell that transmission has ended */
+	char response11[24];
+	sprintf(response11, "226 Directory send OK.\r\n");
+	netconn_write(conn, response11, sizeof(response11), NETCONN_NOCOPY);
+
+	vTaskDelay(50);
+	recv_err = netconn_recv(conn, &inbuf);
+	HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	sprintf(logbuf, "Recv_err: %i\n\r", recv_err);
+	HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+
+	if (recv_err == ERR_OK) {
+		if (netconn_err(conn) == ERR_OK) {
+			netbuf_data(inbuf, (void**)&buf, &buflen);
+			sprintf(logbuf, "Otrzymano %i znakow, wiadomosc: %s\n\r", buflen, buf);
+			HAL_UART_Transmit_IT(huart, logbuf, strlen(logbuf));
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+			vTaskDelay(300);
+		}
+	}
+
 	vTaskDelay(1000);
 	netconn_close(conn);
 	netbuf_delete(inbuf);
