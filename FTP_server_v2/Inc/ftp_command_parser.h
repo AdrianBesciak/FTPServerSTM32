76e85b81bcd3256073b247e433d6ddbd800f45db
Adrian Beściak <adrian.besciak99@gmail.com>
2021-01-10 01:53:05 +0100

    written command_parser

--- FTP_server_v2/Inc/ftp_command_parser.h
+++ FTP_server_v2/Inc/ftp_command_parser.h
@@ -18,10 +18,14 @@ typedef enum ftp_request_type {
 	PWD = 6,
 	BINARY_MODE = 7,
 	PASSIVE_MODE = 8,
-	LIST = 9
+	LIST = 9,
+	NOT_SUPPORTED = 10
 } ftp_request_type;
 
 ftp_request_type get_request_type(const char * request);
+
+void get_user_name(const char * request, char * buffer);
+void get_user_password(const char * request, char * buffer);
 
 
 
--- FTP_server_v2/Src/ftp_command_parser.c
+++ FTP_server_v2/Src/ftp_command_parser.c
@@ -5,6 +5,8 @@
  *      Author: Adik
  */
 
+#include <string.h>
+
 #include "ftp_command_parser.h"
 
 const char * auth_tls = "AUTH TLS\r\n";
@@ -20,5 +22,56 @@ const char * list = "LIST\r\n";
 
 
 ftp_request_type get_request_type(const char * request) {
+	if (strlen(request) == strlen(auth_tls) && strcmp(request, auth_tls) == 0)
+		return AUTH_TLS;
 
+	if (strlen(request) == strlen(auth_ssl) && strcmp(request, auth_ssl) == 0)
+			return AUTH_SSL;
+
+	if (strlen(request) > 4 && strncmp(request, user, 4) == 0)
+		return USER_name;
+
+	if (strlen(request) > 4 && strncmp(request, pass, 4) == 0)
+		return USER_password;
+
+	if (strlen(request) == strlen(syst) && strcmp(request, syst) == 0)
+		return SYSTEM_TYPE;
+
+	if (strlen(request) == strlen(feat) && strcmp(request, feat) == 0)
+		return EXTRA_FEATURES;
+
+	if (strlen(request) == strlen(pwd) && strcmp(request, pwd) == 0)
+		return PWD;
+
+	if (strlen(request) > 4 && strncmp(request, type, 4) == 0) {
+		if (request[5] == 'I')
+			return BINARY_MODE;
+		else
+			return NOT_SUPPORTED;
+	}
+	if (strlen(request) == strlen(pasv) && strcmp(request, pasv) == 0)
+		return PASSIVE_MODE;
+
+	if (strlen(request) == strlen(list) && strcmp(request, list) == 0)
+		return LIST;
+}
+
+/* assumed that buffer has enough length for user_name */
+void get_user_name(const char * request, char * buffer) {
+	uint8_t buff_index = 0;
+	for (int i = 5; i < strlen(request); i++) {
+		if (request[i] == '\r')
+			break;
+		buffer[buff_index] = request[i];
+	}
+}
+
+/*assumed that buffer has enough lenght for password */
+void get_user_password(const char * request, char * buffer) {
+	uint8_t buff_index = 0;
+		for (int i = 5; i < strlen(request); i++) {
+			if (request[i] == '\r')
+				break;
+			buffer[buff_index] = request[i];
+		}
 }
